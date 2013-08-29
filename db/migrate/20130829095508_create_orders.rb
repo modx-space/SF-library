0fcd14e4cfa78cd31a3cb10ce4bf9e0e476d45a5
zxiao <zxiao@successfactors.com>
2013-08-29 18:02:25 +0800

    crate new models:borrow and order

---
+++ db/migrate/20130829095508_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.integer :book_id
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
