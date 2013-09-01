92aed2403dfff3ab146473360cda105682c83afe
zxiao <zxiao@successfactors.com>
2013-09-01 21:21:46 +0800

    finish borrow and order function

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,7 @@
+require "book_controller"
 class BookController < ApplicationController
   
-  before_action :signed_in_user, only: [:new_hot,:index,:borrow_current,:borrow_history,:order_current,:order_history]
+  before_action :signed_in_user, only: [:new_hot,:index,:borrow,:borrow_current,:borrow_history,:order_current,:order_history]
   
   def new_hot
     books = Book.all
@@ -20,15 +21,52 @@ class BookController < ApplicationController
     render 'index'
   end
   
+  def borrow
+    book = Book.find_by(id: params[:book_id])
+    record = Borrow.find_by(user_id: current_user.id, book_id: params[:book_id], status: "使用中")
+    
+    respond_to do |format|
+      if record
+        # 已在使用，不可多借
+        flash[:info] = "你已在使用本书，不可多占资源哦..."
+        format.js
+      else
+        if book.available > 0
+          borrow = Borrow.new
+          borrow.user_id = current_user.id
+          borrow.book_id = params[:book_id]
+          borrow.should_return_date = Time.new + 2.weeks
+          borrow.status = '使用中'
+          borrow.is_expired = 0;
+        
+          if borrow.save
+              book.update_attribute(:store, book.store-1)
+            flash[:success] = "借阅成功!"
+            format.js
+          else
+            # 借阅失败
+            flash[:error] = "借阅失败!"
+            format.js
+          end
+        else
+          # 无库存，可预订
+          flash[:notice] = "无库存,可预订!"
+          format.js
+        end
+      end
+    end
+    
+  end
+  
   def borrow_current
     sql = %Q| select picture,name,isbn,borrows.created_at,
                     should_return_date,is_expired,status
                     from borrows,books
                     where borrows.book_id = books.id
                           and
-                          borrows.user_id = 1
+                          borrows.user_id = #{current_user.id}
             |
-    @borrowing = Borrow.paginate_by_sql(sql,page: params[:page], per_page:3)
+    @borrowing = Borrow.paginate_by_sql(sql,page: params[:page], per_page:10)
     render 'borrowing'
   end
   
@@ -38,20 +76,60 @@ class BookController < ApplicationController
                     from borrows,books
                     where borrows.book_id = books.id
                           and
-                          borrows.user_id = 1
+                          borrows.user_id = #{current_user.id}
+                          and
+                          borrows.status = "已归还"
             |
-    @borrowed = Borrow.paginate_by_sql(sql,page: params[:page], per_page:3)
+    @borrowed = Borrow.paginate_by_sql(sql,page: params[:page], per_page:10)
     render 'borrowed'
   end
   
+  def order
+    book = Book.find_by(id: params[:book_id])
+    record = Borrow.find_by(user_id: current_user.id, book_id: params[:book_id], status: "使用中")
+    
+    respond_to do |format|
+      if record
+        # 已在使用，无需预订
+        flash[:info] = "你已在使用本书，不必预订..."
+        format.js
+      else
+        ordered = Order.find_by(user_id: current_user.id, book_id: params[:book_id], status: "排队中")
+        if ordered
+          # 已预订，无需再次预订
+          flash[:info] = "你已预订过本书，请耐心等候..."
+          format.js
+        else
+          order = Order.new
+          order.user_id = current_user.id
+          order.book_id = params[:book_id]
+          order.status = '排队中'
+          records = Order.find(:all,conditions:{book_id: params[:book_id], status: "排队中"})
+          if order.save
+            book.update_attribute(:store, book.store-1)
+            flash[:success] = "预订成功! 你的服务序号为: #{records.count+1} (^_^)"
+            format.js
+          else
+            # 预订失败
+            flash[:error] = "预订失败!"
+            format.js
+          end
+        end
+      end
+    end
+    
+  end
+  
   def order_current
-    sql = %Q| select picture,name,isbn,orders.created_at,status
+    sql = %Q| select picture,name,isbn,orders.created_at,status,store
                     from orders,books
                     where orders.book_id = books.id
                           and
-                          orders.user_id = 1
+                          orders.user_id = #{current_user.id}
+                          and
+                          orders.status = "排队中"
             |
-    @ordering = Order.paginate_by_sql(sql,page: params[:page], per_page:3)
+    @ordering = Order.paginate_by_sql(sql,page: params[:page], per_page:10)
     render 'ordering'
   end
   
@@ -60,9 +138,11 @@ class BookController < ApplicationController
                     from orders,books
                     where orders.book_id = books.id
                           and
-                          orders.user_id = 1
+                          orders.user_id = #{current_user.id}
+                          and
+                          orders.status = "已处理"
             |
-    @ordered = Order.paginate_by_sql(sql,page: params[:page], per_page:3)
+    @ordered = Order.paginate_by_sql(sql,page: params[:page], per_page:10)
     render 'ordered'
   end
   
