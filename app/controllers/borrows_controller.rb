3c0e8d02d3bd10d2a6701048743760f6c45b5f8d
Ryan (Chao) Deng <rdeng@successfactors.com>
2014-12-01 14:39:50 +0800

    SQL refactoring for order and borrow

--- app/controllers/borrows_controller.rb
+++ app/controllers/borrows_controller.rb
@@ -13,7 +13,7 @@ class BorrowsController < ApplicationController
       	@borrow = current_user.borrows.new
       	@borrow.book_id = params[:book_id]
         @borrow.should_return_date = Time.new + 2.weeks
-        @borrow.status = BORROW_STATUES.index('未出库')
+        @borrow.status = BORROW_STATUSES.index('未出库')
         @borrow.is_expired = 0
         if @borrow.save
             book.update_attribute(:store, book.store-1)
@@ -34,40 +34,28 @@ class BorrowsController < ApplicationController
 
   end
   
-    def borrow_current
-    sql = %Q| select picture,name,isbn,borrows.created_at,
-                    should_return_date,is_expired,borrows.status
-                    from borrows,books
-                    where borrows.book_id = books.id
-                          and
-                          borrows.user_id = #{current_user.id}
-            |
-    @borrowing = Borrow.paginate_by_sql(sql,page: params[:page], per_page: BOOK_PER_PAGE)
+  def borrow_current
+    page = params[:page] || 1
+    @borrows =  Borrow.where("user_id = :user_id and status != ':status'", 
+                      {user_id: current_user.id, status: BORROW_STATUSES.index('已归还')})
+                      .paginate(page: page, per_page: BOOK_PER_PAGE)
     
     respond_to do |format|
-      format.html {render '_borrowing.html.erb'}
+      format.html {render 'index.html.erb'}
 
     end
     
   end
   
   
-    def borrow_history
-    unreturned_index=
-    sql = %Q| select picture,name,isbn,borrows.created_at,
-                    borrows.updated_at
-                    from borrows,books
-                    where borrows.book_id = books.id
-                          and
-                          borrows.user_id = #{current_user.id}
-                          and
-                          borrows.status = #{BORROW_STATUSES.index('已归还')}
-            |
-    @borrowed = Borrow.paginate_by_sql(sql,page: params[:page], per_page: BOOK_PER_PAGE)
+  def borrow_history
+    page = params[:page] || 1
+    @borrows = Borrow.where("user_id = :user_id and status = ':status'", 
+                      {user_id: current_user.id, status: BORROW_STATUSES.index('已归还')})
+                      .paginate(page: page, per_page: BOOK_PER_PAGE)
     
     respond_to do |format|
-      format.html {render '_borrowed.html.erb'}
-      format.js {render 'borrowed.js.erb'}
+      format.html {render 'index.html.erb'}
     end
     
   end
