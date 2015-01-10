2873b64e6f321d177ad57992a34307fb54bea30b
Ryan (Chao) Deng <rdeng@successfactors.com>
2015-01-11 00:29:41 +0800

    add user seat infomation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110125649) do
+ActiveRecord::Schema.define(version: 20150110161444) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -79,6 +79,10 @@ ActiveRecord::Schema.define(version: 20150110125649) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "status"
+    t.string   "building"
+    t.string   "office"
+    t.integer  "seat"
+    t.string   "sf_email"
   end
 
   create_table "votes", force: true do |t|
