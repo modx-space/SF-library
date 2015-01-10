a7be6bda911e4a6f94d9b17b35756b37328c5292
Ryan (Chao) Deng <rdeng@successfactors.com>
2015-01-11 00:09:03 +0800

    Order Search

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,24 @@ class Order < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :book
+
+  def self.search(search, page)
+    if search != nil
+      joins(:book).where('orders.id like ? or books.name like ? or author like ? or isbn like ? or books.category like ? or press like ? or books.tag like ?',
+         "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%").paginate(page: page, per_page: BOOK_PER_PAGE)
+    else
+      paginate(page: page, per_page: BOOK_PER_PAGE)
+    end
+  end
+
+  def self.admin_search(search, page)
+    if search != nil
+      joins(:book, :user).where('orders.id like ? or users.name like ? or users.email like ? or users.team like ? or books.name like ? or author like ? or isbn like ? or books.category like ? or press like ? or books.tag like ?',
+         "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%").paginate(page: page, per_page: BOOK_PER_PAGE)
+    else
+      paginate(page: page, per_page: BOOK_PER_PAGE)
+    end
+  end
   
   def shipout_order
     self.status = :handled
