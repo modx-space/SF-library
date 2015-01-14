440681c0302deb940fa990192ee2622879dccdb7
Ryan Deng <ryan.deng@sap.com>
2015-01-14 13:32:51 +0800

    Add Error_bar and fix bug for create User

--- config/initializers/sys_constants.rb
+++ config/initializers/sys_constants.rb
@@ -1,7 +1,6 @@
 # encoding: UTF-8
 USER_PER_PAGE=10
 BOOK_PER_PAGE=10
-DEFAULT_PASSWORD = '123456'
 Time::DATE_FORMATS[:Y_m_D] = '%Y-%m-%d'
 Time::DATE_FORMATS[:Y_m_D_H_M] = '%Y-%m-%d %H:%M'
 BORROW_PERIOD = 2.weeks
