20f97cec049a4b3848b472e83e273dce7bf0bae7
Ryan Deng <ryan.deng@sap.com>
2015-01-25 00:38:46 +0800

    Feature - sort of Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class Order < ActiveRecord::Base
   extend Enumerize
+  extend SortUtils
 
   enumerize :status, in: [:in_queue, :handled, :canceled], scope: true
 
@@ -9,22 +10,39 @@ class Order < ActiveRecord::Base
 
   validates :status, presence: true
 
-  def self.search(search, page)
-    if search != nil
+  def self.search(search)
+    if search.present?
       joins(:book).where('orders.id like ? or books.name like ? or author like ? or isbn like ? or books.category like ? or press like ? or books.tag like ?',
-         "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%").paginate(page: page, per_page: BOOK_PER_PAGE)
+         "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%")
     else
-      paginate(page: page, per_page: BOOK_PER_PAGE)
+      all
     end
   end
 
-  def self.admin_search(search, page)
-    if search != nil
+  def self.admin_search(search)
+    if search.present?
       joins(:book, :user).where('orders.id like ? or users.name like ? or users.email like ? or users.team like ? or books.name like ? or author like ? or isbn like ? or books.category like ? or press like ? or books.tag like ?',
-         "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%").paginate(page: page, per_page: BOOK_PER_PAGE)
+         "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%")
     else
-      paginate(page: page, per_page: BOOK_PER_PAGE)
+      all
     end
+  end
+
+  def self.sort(sort_type, supported_sort_types)
+    if sort_type.present? && supported_sort_types.include?(sort_type.to_sym)
+      sort_condition = parse_sort_type(sort_type)
+      order(sort_condition)
+    else
+      all
+    end
+  end
+
+  def self.current_sort_types
+    [:created_at_desc]
+  end
+
+  def self.history_sort_types
+    [:created_at_desc, :updated_at_desc]
   end
   
   def shipout_order
