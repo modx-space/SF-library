410e6d479de3a5122ffec654aabbf8d81964895f
Ryan Deng <ryan.deng@sap.com>
2015-04-07 15:15:12 +0800

    Move server to 10.129.126.211

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@10.129.115.210}
-role :web, %w{deploy@10.129.115.210}
-role :db,  %w{deploy@10.129.115.210}
+role :app, %w{deploy@10.129.126.211}
+role :web, %w{deploy@10.129.126.211}
+role :db,  %w{deploy@10.129.126.211}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@10.129.115.210}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '10.129.115.210', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '10.129.126.211', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
