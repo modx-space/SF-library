626a0df8c6d10fb5f2a8a57d1199cf9f007d52d4
Ryan Deng <ryan.deng@sap.com>
2015-04-09 23:44:14 +0800

    gRevert "Change overdue alert to bcc"
    This reverts commit 16ef795e7b90b2610c05638a1505fe1e130bd494.

--- app/mailers/overdue_alert_mailer.rb
+++ app/mailers/overdue_alert_mailer.rb
@@ -1,11 +1,9 @@
 class OverdueAlertMailer < ActionMailer::Base
   default from: "DL_536ACDABDF15DB7F9000000F@exchange.sap.corp" # maybe use other email address, like "minerva.book.lib@sap.com"
 
-  def overdue_alert_to_user user_id_arrary
-    users = User.find user_id_arrary.uniq
-    emails = users.collect(&:email).join(";")
-    @subject = "超期提醒: 您所借的书籍已经超期"
-    mail(bcc: emails, subject: @subject)
+  def overdue_alert_to_user user,book
+    @subject = "超期提醒: 您所借的<#{book.name}>已超期"
+    mail(to: user.email, subject: @subject)
   end
 
   def send_overdue_report_to_admin user, borrow_id_array
--- app/models/overdue_alert.rb
+++ app/models/overdue_alert.rb
@@ -2,8 +2,9 @@ class OverdueAlert < ActiveRecord::Base
 
   belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
 
-  def send_overdue_alert_mail user_id_arrary
-    OverdueAlertMailer.overdue_alert_to_user(user_id_arrary).deliver
+  def send_overdue_alert_mail user, book
+    OverdueAlertMailer.delay(run_at: 10.seconds.from_now)
+      .overdue_alert_to_user(user, book)
   end
 
   def send_overdue_report_mail user, borrow_id_array
@@ -16,13 +17,14 @@ class OverdueAlert < ActiveRecord::Base
         borrows = Borrow.where("should_return_date < :now and status = :status",
                                 { now: Time.now, status: :borrowing })
         borrow_id_array = borrows.pluck(:id)
-        user_id_arrary = borrows.pluck(:user_id)
         self.borrow_ids = borrow_id_array.join(',')
         self.save!
 
-        admin = User.find self.admin_id
-        send_overdue_alert_mail(user_id_arrary) 
-        send_overdue_report_mail(admin, borrow_id_array)
+        user = User.find self.admin_id
+        borrows.all.each{|borrow| 
+          send_overdue_alert_mail(borrow.user, borrow.book)
+        } 
+        send_overdue_report_mail(user, borrow_id_array)
       end
       return {value: :success, message: '邮件已入队列发送'}
     rescue Exception => ex
