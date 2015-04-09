0633de16e2603f25212139e0c18a29b47ddab8bc
Ryan Deng <ryan.deng@sap.com>
2015-04-10 00:04:36 +0800

    Use delayed job to overdue alert

--- app/models/overdue_alert.rb
+++ app/models/overdue_alert.rb
@@ -3,28 +3,31 @@ class OverdueAlert < ActiveRecord::Base
   belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
 
   def send_overdue_alert_mail user, book
-    OverdueAlertMailer.delay(run_at: 10.seconds.from_now)
-      .overdue_alert_to_user(user, book)
+    OverdueAlertMailer.overdue_alert_to_user(user, book).deliver
   end
 
   def send_overdue_report_mail user, borrow_id_array
-    OverdueAlertMailer.send_overdue_report_to_admin(user, borrow_id_array).deliver
+    OverdueAlertMailer.delay.send_overdue_report_to_admin(user, borrow_id_array)
+  end
+
+  def arrange_send_overdue_alert_mail borrows
+    borrows.each do |borrow|
+      send_overdue_alert_mail(borrow.user, borrow.book)
+    end
   end
 
   def save_and_send_mail
     begin
       self.transaction do
         borrows = Borrow.where("should_return_date < :now and status = :status",
-                                { now: Time.now, status: :borrowing })
+                                { now: Time.now, status: :borrowing }).order(id: :desc)
         borrow_id_array = borrows.pluck(:id)
         self.borrow_ids = borrow_id_array.join(',')
         self.save!
 
-        user = User.find self.admin_id
-        borrows.all.each{|borrow| 
-          send_overdue_alert_mail(borrow.user, borrow.book)
-        } 
-        send_overdue_report_mail(user, borrow_id_array)
+        admin = User.find self.admin_id
+        delay.arrange_send_overdue_alert_mail(borrows.all)
+        send_overdue_report_mail(admin, borrow_id_array)
       end
       return {value: :success, message: '邮件已入队列发送'}
     rescue Exception => ex
