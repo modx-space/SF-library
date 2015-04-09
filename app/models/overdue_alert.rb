43483169ec87db58047afed3d1399d00ce70ab72
Ryan Deng <ryan.deng@sap.com>
2015-04-09 22:29:41 +0800

    Change overdue alert to bcc

--- app/models/overdue_alert.rb
+++ app/models/overdue_alert.rb
@@ -2,9 +2,8 @@ class OverdueAlert < ActiveRecord::Base
 
   belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
 
-  def send_overdue_alert_mail user, book
-    OverdueAlertMailer.delay(run_at: 10.seconds.from_now)
-      .overdue_alert_to_user(user, book)
+  def send_overdue_alert_mail user_id_arrary
+    OverdueAlertMailer.overdue_alert_to_user(user_id_arrary).deliver
   end
 
   def send_overdue_report_mail user, borrow_id_array
@@ -17,14 +16,13 @@ class OverdueAlert < ActiveRecord::Base
         borrows = Borrow.where("should_return_date < :now and status = :status",
                                 { now: Time.now, status: :borrowing })
         borrow_id_array = borrows.pluck(:id)
+        user_id_arrary = borrows.pluck(:user_id)
         self.borrow_ids = borrow_id_array.join(',')
         self.save!
 
-        user = User.find self.admin_id
-        borrows.all.each{|borrow| 
-          send_overdue_alert_mail(borrow.user, borrow.book)
-        } 
-        send_overdue_report_mail(user, borrow_id_array)
+        admin = User.find self.admin_id
+        send_overdue_alert_mail(user_id_arrary) 
+        send_overdue_report_mail(admin, borrow_id_array)
       end
       return {value: :success, message: '邮件已入队列发送'}
     rescue Exception => ex
