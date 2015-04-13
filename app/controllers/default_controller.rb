e5e647ef60bbadefea29ceb268dfe8e6637d341a
Ryan Deng <ryan.deng@sap.com>
2015-04-13 15:23:12 +0800

    Change home page admin list order

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -4,7 +4,7 @@ class DefaultController < ApplicationController
   
   def home
     @books_new = Book.new_book_list
-    @admins = User.on_board.with_role(:super_admin, :admin)
+    @admins = User.on_board.with_role(:super_admin, :admin).order(role: :desc)
   end
   
 end
